ebe3ac7e640e65f9693a146128da8b5cc73cdbc0
Nathan Weetman <nathan@weetman.net>
2024-01-07 17:53:35 +0000

    fix: Define sigpipe SIG_IGN in braces to avoid compiler warning

--- main.cpp
+++ main.cpp
@@ -101,7 +101,7 @@ int main(int argc, char **argv){
     bool toggle = true;
     setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &toggle, sizeof(int)); // To avoid re-binding issues
     // Ignore SIGPIPE
-    struct sigaction sigpipe { SIG_IGN };
+    struct sigaction sigpipe { { SIG_IGN } };
     sigaction(SIGPIPE, &sigpipe, NULL);
 
     memset((char *) &serv_addr, 0, sizeof(serv_addr));
